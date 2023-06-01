95c8485675136a180ee6170b98bf48e7856d4ba4
khokharaagna <aagnakhokhar89@gmail.com>
2023-06-01 17:12:57 +0530

    third commit

---
+++ sum.c/SUM.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int i,n,sum=0;
+printf("enter any number:");
+scanf("%d", &n);
+for(i=1;i<=n;i++);
+{
+sum=sum+i;
+}
+printf("sum of %d natural number is %d",n,sum);
+getch();
+}
