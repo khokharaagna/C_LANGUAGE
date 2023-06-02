ea0589f7c5694cf9ff3f761c7d6dbec558a8141e
khokharaagna <aagnakhokhar89@gmail.com>
2023-06-02 14:32:25 +0530

    pr commit

---
+++ pr3_c/alphabet1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    int a=65;
+    while(a<=90)
+    {
+
+    if(a!=66 && a!=67 && a!=68){
+    printf("%c  ",a);
+
+    }
+    a++;
+    }
+    return 0;
+}
