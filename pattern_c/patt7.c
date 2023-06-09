3d3b3f508b8446357d7dbaca58e2060be73ed79a
khokharaagna <aagnakhokhar89@gmail.com>
2023-06-09 17:11:10 +0530

    pattern loop

---
+++ pattern_c/patt7.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+    int i,j;
+    char ch= 'A';
+    for(i=1;i<=5;i++){
+ch=ch+i;
+  for(j=i;j>=1;j--){
+    ch--;
+   printf("%c",ch);
+
+  }
+ch='A';
+  printf("\n");
+    }
+    getch();
+}
