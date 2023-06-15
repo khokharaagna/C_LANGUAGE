820a26864d43599d9e45459dde98578c317b9779
khokharaagna <aagnakhokhar@gmail.com>
2023-06-15 14:22:48 +0530

    PR Commit

---
+++ prcirclet4_c/pr8.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    int i;
+    for(i=1;i<=7;i++){
+   if(i==1){
+printf("    *\n");    
+}else if(i==2){
+printf("  *   *\n");
+}else if(i==3){
+printf(" *     *\n");
+}else if(i==4){
+printf("*      *\n");
+}else if(i==5){
+printf("*     *\n");
+}else if(i==6){
+printf("   *\n");
+}else{
+printf("      *");
+    
+}     
+    }
+    return 0;
+}
