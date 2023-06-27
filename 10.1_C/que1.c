f13a0eaf07e40cd52b7e4b01dd8194871dcf1775
khokharaagna <aagnakhokhar@gmail.com>
2023-06-27 14:15:18 +0530

    task commit

---
+++ 10.1_C/que1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void message(int n){
+	
+	printf("Enter the number :");
+	scanf("%d",&n);
+	n=n*n*n;
+	
+	
+	printf("the answer is: %d",n);
+    }
+	void main(){
+		int a;
+		message(a);     
+	}
+
