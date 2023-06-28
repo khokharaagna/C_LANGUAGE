ac2020d05307eb9529c91cdb6ae4c5f2b12814d3
khokharaagna <aagnakhokhar@gmail.com>
2023-06-28 14:58:12 +0530

    exercise commit

---
+++ 10.2_c/length.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void modify(char[]);
+
+void main()
+{
+  char str1[]="aagna";
+  	
+   modify(str1);
+}
+
+void modify(char str1[])
+{
+  int i,l=0;
+  for(i=0;str1[i]!='\0';i++)
+  {
+    l=l+1;
+  }
+  str1[0]='a';
+  printf("length of string 1: is %d",l);
+}
